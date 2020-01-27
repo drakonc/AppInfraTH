017cb71553be51283434c9f4101e31cedd779b62
Jose Castro <drakonc@gmail.com>
2020-01-27 14:36:40 -0500

    .

--- src/App/Models/tasksModel.ts
+++
@@ -1,8 +0,0 @@
-import { Schema, model } from "mongoose";
-
-const TaskSchema = new Schema({
-    titulo: { type: String, required: true, lowercase: true },
-    descripcion: { type: String, required: true, lowercase: true }
-});
-
-export default model('Task', TaskSchema);
